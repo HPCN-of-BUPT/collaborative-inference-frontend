430aa2e1821615b4a4b2e23bde410767ab215e6b
RoyXinyun <zhangxinyun99@163.com>
2021-08-27 01:47:42 +0800

    fix

--- src/views/arrange.vue
+++ src/views/arrange.vue
@@ -78,15 +78,12 @@
                     <el-col :span="6">
                       <span class="font_style">云端口号</span>
                     </el-col>
-
                     <el-col :span="18" style="text-align: left">
-                      <el-input v-model="cloud_port" placeholder="请输入内容" class="input"></el-input>
+                    <el-select v-model="cloud_port" placeholder="默认端口" class="input">
+                      <el-option label="默认端口" value="1"></el-option>
+                    </el-select>
                     </el-col>
                   </el-row>
-                </div>
-
-                <div class="input_model" style="text-align: center">
-                  <el-button type="primary" size="small" @click="cloud_deploy" >云模型部署</el-button>
                 </div>
 
                 <div class="input_model">
@@ -108,15 +105,25 @@
                     </el-col>
 
                     <el-col :span="18" style="text-align: left">
-                      <el-input v-model="edge_port" placeholder="请输入内容" class="input"></el-input>
+                      <el-select v-model="cloud_port" placeholder="默认端口" class="input">
+                        <el-option label="默认端口" value="1"></el-option>
+                      </el-select>
                     </el-col>
                   </el-row>
                 </div>
 
-                <div class="input_model" style="text-align: center">
-                  <el-button type="primary" size="small" @click="edge_deploy" >边模型部署</el-button>
-                </div>
+                <div class="input_model" >
+                  <el-row :gutter="20">
+                    <el-col :span="12" style="text-align: right">
+                      <el-button type="primary" size="small" @click="cloud_deploy" >云模型部署</el-button>
+                    </el-col>
 
+                    <el-col :span="12" style="text-align: left">
+                      <el-button type="primary" size="small" @click="edge_deploy" >边模型部署</el-button>
+                    </el-col>
+                  </el-row>
+
+                </div>
 
               </el-card>
         </div>
@@ -129,6 +136,8 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "arrange",
   data() {
@@ -141,10 +150,6 @@ export default {
       cloud_params_file: [],
       edge_frame_file: [],
       edge_params_file: [],
-      fileList: [{
-        name: 'food.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-      }],
       model_type:'',
       res_type:''
     };
@@ -159,98 +164,91 @@ export default {
         this.$router.push('test')
       }
     },
-    cloud_true_upload1(){
-      this.$refs.cloud_upload1.click()
-    },
-    cloud_true_upload2(){
-      this.$refs.cloud_upload2.click()
-    },
-    edge_true_upload1(){
-      this.$refs.edge_upload1.click()
-    },
-    edge_true_upload2(){
-      this.$refs.edge_upload2.click()
-    },
-    cloud_upload_frame(event){
-      var file = event.target.files[0]
-      let param = new FormData(); //创建form对象
-      param.append('file',file);//通过append向form对象添加数据
-      console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
-      let config = {
-        headers:{'Content-Type':'multipart/form-data'}
-      }; //添加请求头
-      this.$axios.post('api' + '/cloud_upload_frame',param,config)
-        .then((response) => {
-          this.$message.success(response.data.result)
-        })
-    },
-    cloud_upload_params(event){
-      var file = event.target.files[0]
-      let param = new FormData(); //创建form对象
-      param.append('file',file);//通过append向form对象添加数据
-      console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
-      let config = {
-        headers:{'Content-Type':'multipart/form-data'}
-      }; //添加请求头
-      this.$axios.post('api' + '/cloud_upload_params',param,config)
-        .then((response) => {
-          this.$message.success(response.data.result)
-        })
-    },
+    // cloud_true_upload1(){
+    //   this.$refs.cloud_upload1.click()
+    // },
+    // cloud_true_upload2(){
+    //   this.$refs.cloud_upload2.click()
+    // },
+    // edge_true_upload1(){
+    //   this.$refs.edge_upload1.click()
+    // },
+    // edge_true_upload2(){
+    //   this.$refs.edge_upload2.click()
+    // },
+    // cloud_upload_frame(event){
+    //   var file = event.target.files[0]
+    //   let param = new FormData(); //创建form对象
+    //   param.append('file',file);//通过append向form对象添加数据
+    //   console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+    //   let config = {
+    //     headers:{'Content-Type':'multipart/form-data'}
+    //   }; //添加请求头
+    //   this.$axios.post('api' + '/cloud_upload_frame',param,config)
+    //     .then((response) => {
+    //       this.$message.success(response.data.result)
+    //     })
+    // },
+    // cloud_upload_params(event){
+    //   var file = event.target.files[0]
+    //   let param = new FormData(); //创建form对象
+    //   param.append('file',file);//通过append向form对象添加数据
+    //   console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+    //   let config = {
+    //     headers:{'Content-Type':'multipart/form-data'}
+    //   }; //添加请求头
+    //   this.$axios.post('api' + '/cloud_upload_params',param,config)
+    //     .then((response) => {
+    //       this.$message.success(response.data.result)
+    //     })
+    // },
     cloud_deploy(){
       axios({
-        methods: "post",
-        url:"",
-        data:[{"cloud_ip":this.cloud_ip,"cloud_port":this.cloud_port}]
+        method: "POST",
+        url:'api' + '/cloud_model_arrange',
+        data:{"cloud_ip":this.cloud_ip},
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type,Authorization"}
       }).then((response) => {
-        if (response.data.result == "failure") {
-          this.$message.error("连接失败!");
-        } else {
-          this.$message.success("连接成功!");
-        }
+
       })
 
-    },
-    edge_upload_frame(event){
-      var file = event.target.files[0]
-      let param = new FormData(); //创建form对象
-      param.append('file',file);//通过append向form对象添加数据
-      console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
-      let config = {
-        headers:{'Content-Type':'multipart/form-data'}
-      }; //添加请求头
-      this.$axios.post('api' + '/edge_upload_frame',param,config)
-        .then((response) => {
-          this.$message.success(response.data.result)
-        })
-    },
-    edge_upload_params(event){
-      var file = event.target.files[0]
-      let param = new FormData(); //创建form对象
-      param.append('file',file);//通过append向form对象添加数据
-      console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
-      let config = {
-        headers:{'Content-Type':'multipart/form-data'}
-      }; //添加请求头
-      this.$axios.post('api' + '/edge_upload_params',param,config)
-        .then((response) => {
-          this.$message.success(response.data.result)
-        })
     },
     edge_deploy(){
       axios({
-        methods: "post",
-        url:"",
-        data:[{"edge_ip":this.edge_ip,"edge_port":this.edge_port}]
+        method: "POST",
+        url:'api' + '/edge_model_arrange',
+        data:{"edge_ip":this.edge_ip},
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type,Authorization"}
       }).then((response) => {
-        if (response.data.result == "failure") {
-          this.$message.error("连接失败!");
-        } else {
-          this.$message.success("连接成功!");
-        }
-      })
 
-    }
+      })
+    },
+    // edge_upload_frame(event){
+    //   var file = event.target.files[0]
+    //   let param = new FormData(); //创建form对象
+    //   param.append('file',file);//通过append向form对象添加数据
+    //   console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+    //   let config = {
+    //     headers:{'Content-Type':'multipart/form-data'}
+    //   }; //添加请求头
+    //   this.$axios.post('api' + '/edge_upload_frame',param,config)
+    //     .then((response) => {
+    //       this.$message.success(response.data.result)
+    //     })
+    // },
+    // edge_upload_params(event){
+    //   var file = event.target.files[0]
+    //   let param = new FormData(); //创建form对象
+    //   param.append('file',file);//通过append向form对象添加数据
+    //   console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+    //   let config = {
+    //     headers:{'Content-Type':'multipart/form-data'}
+    //   }; //添加请求头
+    //   this.$axios.post('api' + '/edge_upload_params',param,config)
+    //     .then((response) => {
+    //       this.$message.success(response.data.result)
+    //     })
+    // }
   }
 }
 </script>
